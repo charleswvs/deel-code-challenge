fb493ab200c8c287d42cd3dc6b2f96dc8e91db6b
Charles Willian <wcharlesw15@gmail.com>
2022-09-17 13:50:06 -0300

    chore: removes unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,6 @@ import "./App.css";
 import AutoComplete from "./components/AutoComplete/AutoComplete";
 import { FilterDataInput } from "./lib/filterDataByQuery";
 
-const options = [
-  { id: 1, value: "apples" },
-  { id: 2, value: "banana" },
-  { id: 3, value: "cherry" },
-  { id: 4, value: "watermelon" },
-  { id: 5, value: "peach" },
-  { id: 6, value: "pumpkin" },
-  { id: 7, value: "tomato" },
-  { id: 8, value: "café" },
-];
-
 function App() {
   const [selectedItem, setSelectedItem] = useState<FilterDataInput>();
 
